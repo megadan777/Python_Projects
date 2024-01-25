02fe69fc0929ee3b3530bd8af62af5318913074a
Daniel Mowbray <daniel.mowbray86@gmail.com>
2024-01-25 19:54:05 +1100

    2501
    Copying the pdfs work, not translating to audio yet

--- Python_Projects/PDF_Audio/pdfaudio.py
+++ Python_Projects/PDF_Audio/pdfaudio.py
@@ -3,26 +3,37 @@
 import pyttsx3,PyPDF2
 
 #Copy PDF files from one folder to another
-import glob
 import shutil
 import os
-    
-source_dir = '/Desktop/PDFS'
-target_dir = '/Desktop/Programming/Python_Projects/PDF_Audio'
-    
+
+# Get the user's desktop directory
+desktop_dir = os.path.join(os.path.expanduser("~"), "Desktop")
+
+source_dir = os.path.join(desktop_dir, 'PDFS')
+target_dir = os.path.join(desktop_dir, 'Programming', 'Python_Projects', 'PDF_Audio')
+
+# Check if the source directory exists
+if not os.path.exists(source_dir):
+    print(f"Source directory '{source_dir}' not found.")
+    exit()
+
 file_names = os.listdir(source_dir)
-    
+
 for file_name in file_names:
-    if file not in glob.glob(os.path.join(target_dir, GLOB_PARMS)):
-        shutil.copy(os.path.join(source_dir, file_names), target_dir)
+    source_file_path = os.path.join(source_dir, file_name)
+    target_file_path = os.path.join(target_dir, file_name)
+    
+    if not os.path.exists(target_file_path):
+        shutil.copy(source_file_path, target_dir)
     else:
-    print("{} exists in {}".format(
-        file,os.path.join(os.path.split(target_dir)[-2:])))
+        print(f"{file_name} exists in {os.path.abspath(target_dir)}")
+
+
 # This is just a print command that outputs to console that the
 # file was already in director
 
 #insert name of your pdf 
-pdfreader = PyPDF2.PdfFileReader(open('book.pdf', 'rb'))
+pdfreader = PyPDF2.PdfFileReader(open('The_Subsidiary_Hierarchy.pdf', 'rb'))
 speaker = pyttsx3.init()
 
 for page_num in range(pdfreader.numPages):
